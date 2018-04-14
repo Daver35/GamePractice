963981428c19d7c62db2003eb454d8d58937f742
Guillem Molina Galera <guillem_3_8@hotmail.com>
2018-04-14 18:53:27 +0200

    Add Eenemy
    Added an Enemy which follows the Player.
    Added a platform to walk on

---
+++ ZombieSurvival/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+
+    public float moveSpeed;
+    Transform target;
+
+    // Use this for initialization
+    void Start () {
+        target = GameObject.FindWithTag("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //Calculate direction
+        float dir = target.position.x - GetComponent<Rigidbody2D>().position.x;
+
+        if (dir > 0)//Right
+        {
+
+            GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        }
+        if (dir < 0)//Left
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        }
+
+    }
+}
